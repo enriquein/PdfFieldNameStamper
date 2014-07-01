0f04722be8d262afdf23ebcb00ba97ed186a93ac
Enrique Ramirez <enriquein@gmail.com>
2014-07-01 16:22:14 -0400

    Application can now be launched to run via command-line. Implements #2.

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,12 +9,25 @@ namespace PdfFieldNameStamper
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        [DllImport("kernel32.dll")]
+        private static extern bool AttachConsole(int pid);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool FreeConsole();
+
         [STAThread]
         static void Main()
         {
+            var args = Environment.GetCommandLineArgs().ToList();
+            if (args.Count > 1)
+            {
+                AttachConsole(-1);
+                ConsoleRunner.Run(args);
+                FreeConsole();
+                SendKeys.SendWait("{ENTER}");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
